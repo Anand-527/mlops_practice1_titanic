13d26dfd2fc9e34c4b192b3b47eb637cc7b6f6f7
Anand-527 <valabojuanandchary@gmail.com>
2022-03-07 21:51:01 +0530

    before building the app - buit package but not uploaded to pyindex or gemfury

--- Modelling/Features.py
+++ Modelling/Features.py
@@ -1,115 +1,119 @@
+import re
 
-from sklearn.base import BaseEstimator, TransformerMixin
 import numpy as np
+from sklearn.base import BaseEstimator, TransformerMixin
 
 
-
-class ReplacingWithNan(BaseEstimator,TransformerMixin):
+class ReplacingWithNan(BaseEstimator, TransformerMixin):
     # Extract fist letter of variable
 
-    def __init__(self,var = None):
+    def __init__(self, var=None):
         self.var = None
-    
+
     def fit(self, X, y=None):
         return self
 
-    def transform(self,X):
+    def transform(self, X):
         X = X.copy()
 
-        X = X.replace('?',np.nan)
-        
+        X = X.replace("?", np.nan)
+
         return X
 
 
-
-class FeatureDropping(BaseEstimator,TransformerMixin):
+class FeatureDropping(BaseEstimator, TransformerMixin):
     # Extract fist letter of variable
 
     def __init__(self, variables):
-        if not isinstance(variables,list):
-            raise ValueError('variables should be a list')
+        if not isinstance(variables, list):
+            raise ValueError("variables should be a list")
 
         self.variables = variables
-        
+
     def fit(self, X, y=None):
         return self
 
-    def transform(self,X):
+    def transform(self, X):
         X = X.copy()
-        X = X.drop(labels = self.variables, axis=1)
-        
+        X = X.drop(labels=self.variables, axis=1)
+
         return X
 
 
-
-class CovertingToFloat(BaseEstimator,TransformerMixin):
+class CovertingToFloat(BaseEstimator, TransformerMixin):
     # Extract fist letter of variable
 
     def __init__(self, variables):
-        if not isinstance(variables,list):
-            raise ValueError('variables should be a list')
+        if not isinstance(variables, list):
+            raise ValueError("variables should be a list")
 
         self.variables = variables
-        
+
     def fit(self, X, y=None):
         return self
 
-    def transform(self,X):
+    def transform(self, X):
         X = X.copy()
 
         for i in self.variables:
-            X[i] = X[i].astype('float')
-        
+            X[i] = X[i].astype("float")
+
         return X
 
 
-
-class SalutationExtraction(BaseEstimator,TransformerMixin):
+class SalutationExtraction(BaseEstimator, TransformerMixin):
     # Extract fist letter of variable
 
     def __init__(self, variables):
-        if not isinstance(variables,list):
-            raise ValueError('variables should be a list')
+        if not isinstance(variables, list):
+            raise ValueError("variables should be a list")
 
         self.variables = variables
-        
+
     def fit(self, X, y=None):
         return self
 
-    def transform(self,X):
+    def transform(self, X):
         X = X.copy()
-    
-        X['title']  = X[self.variables].apply(lambda x:
-        'Mrs' if 'Mrs' in x else(
-            'Mr' if 'Mr' in x else(
-                'Miss' if 'Miss' else(
-                    'Master' if 'Master' in X else 'Other'
-        ))))
-        
+
+        def get_title(passenger):
+            line = passenger
+            if re.search("Mrs", line):
+                return "Mrs"
+            elif re.search("Mr", line):
+                return "Mr"
+            elif re.search("Miss", line):
+                return "Miss"
+            elif re.search("Master", line):
+                return "Master"
+            else:
+                return "Other"
+
+        X["title"] = X[self.variables[0]].apply(get_title)
+
         return X
 
 
-
-
-class ExtractLetterTransformer(BaseEstimator,TransformerMixin):
+class ExtractLetterTransformer(BaseEstimator, TransformerMixin):
     # Extract fist letter of variable
 
     def __init__(self, variables):
-        if not isinstance(variables,list):
-            raise ValueError('variables should be a list')
+        if not isinstance(variables, list):
+            raise ValueError("variables should be a list")
 
         self.variables = variables
-        
+
     def fit(self, X, y=None):
         return self
 
-    def transform(self,X):
+    def transform(self, X):
         X = X.copy()
 
         for feature in self.variables:
             X[feature] = X[feature].str[0]
-        
+
         return X
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     pass
--- Modelling/Load_Data.py
+++ Modelling/Load_Data.py
@@ -1,41 +1,56 @@
-from importlib.resources import path
-import pandas as pd
-import yaml
-import pathlib
 from pathlib import Path
+
 import joblib
+import pandas as pd
+from Config.config_validations import Config
 from sklearn.pipeline import Pipeline
 
-with open('config.yaml','r') as file:
-    y = yaml.safe_load(file)
+cd = Path(__file__).parent
+version_file_path = Path.joinpath(cd, "VERSION")
 
-def data():
-    data = pd.read_csv('https://www.openml.org/data/get_csv/16826755/phpMYEkMl')
+with open(version_file_path) as file:
+    ver = file.read()
+
+cd = Path(__file__).parent
+data_file_path = Path.joinpath(cd.parent, "Datasets\\data.csv")
+model_file_name = (
+    "Trained_models\\" + Config.model_params.Model_name + "_v" + str(ver) + ".pkl"
+)
+
+
+def data() -> pd.DataFrame:
+    data = pd.read_csv(data_file_path)
     return data
 
-def save_model_pipeline(model_pipeline_to_save:Pipeline):
-    file_name = 'Trained_models\\' + y['Model_name']+'.pkl'
-    complete_file_name = Path.joinpath(Path.cwd(),file_name)
+
+def save_model_pipeline(model_pipeline_to_save: Pipeline):
+    complete_file_name = Path.joinpath(cd, model_file_name)
     remove_old_model_pipeline()
-    joblib.dump(model_pipeline_to_save,complete_file_name)
+    joblib.dump(model_pipeline_to_save, complete_file_name)
+
 
 def load_model_pipeline():
-    file_name = 'Trained_models\\' + y['Model_name']+'.pkl'
-    complete_file_name = Path.joinpath(Path.cwd(),file_name)
+    complete_file_name = Path.joinpath(cd, model_file_name)
     return joblib.load(complete_file_name)
 
+
 def remove_old_model_pipeline():
-    path_name = Path.joinpath(Path.cwd(),'Trained_models')
-    #print(path_name)
+    path_name = Path.joinpath(cd, "Trained_models")
+    # print(path_name)
     for i in Path(path_name).iterdir():
-        print(i)
-        if '.pkl' in str(i):
+        # print(i)
+        if i != model_file_name:
             i.unlink()
+        else:
+            raise Exception("File already exists!")
 
 
-if __name__ == '__main__':
-    #print(remove_model_pipeline())
-    pass
+if __name__ == "__main__":
+    # print(remove_model_pipeline())
+    print(data().head(5))
+    # print(v)
+    # pass
 
 
+# python Production/Modelling/Load_Data.py
 # python Load_Data.py
